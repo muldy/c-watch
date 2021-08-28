d9c185e9674b0e1ab30c92466cb817bc96fccd08
muldy <muldy@ryzen>
2021-08-28 14:58:03 +0100

    more waftools

--- waftools/defines.py
+++ waftools/defines.py
@@ -1,7 +1,8 @@
 def options(ctx):
-    ctx.add_option('--no-logs',action='store_true',default=False,
-            help="Mark a build to exclude app logging output")
+    ctx.add_option('--no-logs', action='store_true', default=False,
+                   help="Mark a build to exclude app logging output")
+
 
 def configure(ctx):
     if ctx.options.no_logs:
-        ctx.env.append_value('DEFINES','NOLOG')
+        ctx.env.append_value('DEFINES', 'NOLOG')
